c610f9784c6407406596be25d1c8f0e5869d851f
Prabhat Shahi <pra_shahi@hotmail.com>
2020-05-26 09:20:25 +0545

    required email confirmation for new thread page - resolve #1

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('threads', 'ThreadsController@index')->name('threads');
-Route::get('threads/create', 'ThreadsController@create');
+Route::get('threads/create', 'ThreadsController@create')->middleware('verified');
 Route::get('threads/search', 'SearchController@show');
 Route::get('threads/{channel}/{thread}', 'ThreadsController@show');
 Route::patch('threads/{channel}/{thread}', 'ThreadsController@update');
